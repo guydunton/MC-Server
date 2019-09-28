a1c6877bfc0a0c89652b4787cb937c5324319c51
Guy Dunton <guy.dunton@gmail.com>
2019-09-28 09:01:19 +0100

    Simplify deploy-resources.sh
    * Changed deploy resources script to upload all files in
    server-resources to s3 rather than named files.
    * Allowed passing in variables to the script for uploading "all" files.
    Syntax is UPLOAD_ALL=all ./deploy-resources.sh

--- deploy-resources.sh
+++ deploy-resources.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -eu
 
-UPLOAD_ALL=""
+UPLOAD=${UPLOAD_ALL:-}
 
-if [ UPLOAD_ALL == "all" ]; then
+if [[ $UPLOAD == "all" ]]; then
     curl https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar > server.jar
     aws s3 mv ./server.jar s3://guydunton-mc-resources/
 fi
 
-aws s3 cp ./server-resources/mc-instance-start-script.sh s3://guydunton-mc-resources/
-aws s3 cp ./server-resources/minecraft.service s3://guydunton-mc-resources/
+aws s3 cp server-resources s3://guydunton-mc-resources --recursive
