c3817c5d2fab60380fc9a214f7cad72d0d5941fb
Guy Dunton PC <guy.dunton@gmail.com>
2019-10-06 16:03:53 +0100

    Add check for the weekend

--- scheduling-stack/src/app.ts
+++ scheduling-stack/src/app.ts
@@ -1,20 +1,28 @@
-import * as aws from "aws-sdk";
+import * as aws from 'aws-sdk';
 const EC2 = new aws.EC2({
-  region: "eu-west-2"
+  region: 'eu-west-2'
 });
 
-type State = "ON" | "OFF";
+type State = 'ON' | 'OFF';
 type InstanceState =
-  | "pending"
-  | "running"
-  | "shutting-down"
-  | "terminated"
-  | "stopping"
-  | "stopped";
+  | 'pending'
+  | 'running'
+  | 'shutting-down'
+  | 'terminated'
+  | 'stopping'
+  | 'stopped';
 
 interface StateAtTime {
   time: number;
   state: State;
+}
+
+function ok() {
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'text/plain' },
+    body: 'Completed evecution'
+  };
 }
 
 export const handler = async function(event: any) {
@@ -22,7 +30,13 @@ export const handler = async function(event: any) {
   let now = new Date();
   let currentHour = now.getHours();
 
-  console.log("Current Time: ", now);
+  console.log('Current Time: ', now);
+
+  // If it's the weekend then leave the instance as is
+  const day = getDayString(now.getDay());
+  if (day === 'Saturday' || day === 'Sunday') {
+    return ok();
+  }
 
   // Get the instance schedule
   const schedule = getSchedule();
@@ -32,7 +46,7 @@ export const handler = async function(event: any) {
     ({ time }) => time === currentHour
   );
 
-  let desiredState = "OFF";
+  let desiredState = 'OFF';
   if (desiredStateOrUndefined !== undefined) {
     desiredState = desiredStateOrUndefined.state;
   }
@@ -40,15 +54,15 @@ export const handler = async function(event: any) {
   try {
     // Get the state of the server
     let instances = await EC2.describeInstances({
-      Filters: [{ Name: "tag:Name", Values: ["MC Server"] }]
+      Filters: [{ Name: 'tag:Name', Values: ['MC Server'] }]
     }).promise();
 
     const instanceState = getInstanceState(instances);
 
     if (instanceState !== undefined) {
       // if the instance is on and supposed to be off
-      if (instanceState.state === "ON" && desiredState === "OFF") {
-        console.log("Stopping instance");
+      if (instanceState.state === 'ON' && desiredState === 'OFF') {
+        console.log('Stopping instance');
         // Turn if off
         await EC2.stopInstances({
           InstanceIds: [instanceState.instanceName]
@@ -56,59 +70,72 @@ export const handler = async function(event: any) {
       }
 
       // if the instance is off and supposed to be on
-      if (instanceState.state === "OFF" && desiredState === "ON") {
-        console.log("Starting instance");
+      if (instanceState.state === 'OFF' && desiredState === 'ON') {
+        console.log('Starting instance');
         // Turn it on
         await EC2.startInstances({
           InstanceIds: [instanceState.instanceName]
         }).promise();
       }
     } else {
-      console.log("Unable to find instance");
+      console.log('Unable to find instance');
     }
   } catch (err) {
     console.error(`Server error occured ${err}`);
 
     return {
       statusCode: 500,
-      headers: { "Content-Type": "text/plain" },
+      headers: { 'Content-Type': 'text/plain' },
       body: `Server error occured: ${err}`
     };
   }
 
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: "Completed evecution"
-  };
+  return ok();
 };
+
+/**
+ * Convert a day number into the name of the day
+ * @param day Must come from Date::getDay()
+ */
+function getDayString(day: number) {
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ];
+  return days[day];
+}
 
 function getSchedule(): StateAtTime[] {
   return [
-    { time: 0, state: "OFF" },
-    { time: 1, state: "OFF" },
-    { time: 2, state: "OFF" },
-    { time: 3, state: "OFF" },
-    { time: 4, state: "OFF" },
-    { time: 5, state: "OFF" },
-    { time: 6, state: "OFF" },
-    { time: 7, state: "OFF" },
-    { time: 8, state: "OFF" },
-    { time: 9, state: "OFF" },
-    { time: 10, state: "OFF" },
-    { time: 11, state: "OFF" },
-    { time: 12, state: "OFF" },
-    { time: 13, state: "OFF" },
-    { time: 14, state: "OFF" },
-    { time: 15, state: "OFF" },
-    { time: 16, state: "ON" },
-    { time: 17, state: "ON" },
-    { time: 18, state: "ON" },
-    { time: 19, state: "ON" },
-    { time: 20, state: "ON" },
-    { time: 21, state: "ON" },
-    { time: 22, state: "ON" },
-    { time: 23, state: "OFF" }
+    { time: 0, state: 'OFF' },
+    { time: 1, state: 'OFF' },
+    { time: 2, state: 'OFF' },
+    { time: 3, state: 'OFF' },
+    { time: 4, state: 'OFF' },
+    { time: 5, state: 'OFF' },
+    { time: 6, state: 'OFF' },
+    { time: 7, state: 'OFF' },
+    { time: 8, state: 'OFF' },
+    { time: 9, state: 'OFF' },
+    { time: 10, state: 'OFF' },
+    { time: 11, state: 'OFF' },
+    { time: 12, state: 'OFF' },
+    { time: 13, state: 'OFF' },
+    { time: 14, state: 'OFF' },
+    { time: 15, state: 'OFF' },
+    { time: 16, state: 'ON' },
+    { time: 17, state: 'ON' },
+    { time: 18, state: 'ON' },
+    { time: 19, state: 'ON' },
+    { time: 20, state: 'ON' },
+    { time: 21, state: 'ON' },
+    { time: 22, state: 'ON' },
+    { time: 23, state: 'OFF' }
   ];
 }
 
@@ -127,7 +154,7 @@ function getInstanceState(
           if (
             instance.Tags &&
             instance.Tags.find(
-              ({ Key, Value }) => Key === "Name" && Value === "MC Server"
+              ({ Key, Value }) => Key === 'Name' && Value === 'MC Server'
             ) &&
             instance.State &&
             instance.State.Name &&
@@ -137,11 +164,11 @@ function getInstanceState(
             const state = instance.State.Name as InstanceState;
 
             switch (state) {
-              case "pending":
-              case "running":
-                return { state: "ON", instanceName };
+              case 'pending':
+              case 'running':
+                return { state: 'ON', instanceName };
               default:
-                return { state: "OFF", instanceName };
+                return { state: 'OFF', instanceName };
             }
           }
         }
